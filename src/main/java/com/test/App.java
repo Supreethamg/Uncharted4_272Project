61e7708a49328fd9a6a5e0735aad1f06fc799dde
Supreethamg <supreethamg@gmail.com>
2017-12-06 01:22:28 -0800

    test code
    commiting test code

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.UUID;
+
+/**
+ *
+Hello world  sample 
+ Testing github integration and auto detecting git commit.
+ TO check if git detects push and triggers build in jenkins successfully.
+The build was successfull..
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!!");
+    }
+    
+    
+}
